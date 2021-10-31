58498ecb58d5d8d14a65d72dc48542152856d05f
DyatlovIvan <diatlovivan92@mail.ru>
2021-10-31 19:31:43 +0400

    contaniner components storeContext

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store, {RootStoreType} from './redux/redux-store'
+import store, {RootStoreType, StoreType} from './redux/redux-store'
 import {BrowserRouter} from 'react-router-dom';
+
 
 export const renderEntireTree = (state: RootStoreType) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App store={store} />
+                <App
+                    store={store}
+                />
         </BrowserRouter>,
         document.getElementById('root')
     );
