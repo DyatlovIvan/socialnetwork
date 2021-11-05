84186e6146abf19cacbf9a0f2a27d3226882d9b9
DyatlovIvan <diatlovivan92@mail.ru>
2021-11-05 23:20:11 +0400

    49

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,8 +1,47 @@
-export const Users = () =>{
+import {UsersType} from "../../redux/usersReducer";
+import s from './Users.module.css'
 
-    return(
+
+type UsersPresentType = {
+    users: Array<UsersType>
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+    setUsers: (users: Array<UsersType>) => void
+}
+export const Users = (props: UsersPresentType) => {
+
+    return (
         <div>
-            dddd
+            {props.users.map(el => <div key={el.id}>
+                <span>
+                    <div>
+                        <img className={s.usersPhoto} src={el.photoUrl}/>
+                    </div>
+                    <div>
+                        {el.followed ?
+                            <button onClick={() => props.unfollow(el.id)}>Unfollow</button>
+                            : <button onClick={() => props.follow(el.id)}>Follow</button>}
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>
+                            {el.fullName}
+                        </div>
+                        <div>
+                            {el.status}
+                        </div>
+                    </span>
+                    <span>
+                        <div>
+                            {el.location.country}
+                        </div>
+                        <div>
+                            {el.location.city}
+                        </div>
+                    </span>
+                </span>
+            </div>)}
         </div>
     )
 }
--- src/redux/usersReducer.tsx
+++ src/redux/usersReducer.tsx
@@ -7,6 +7,7 @@ export type UsersType = {
     id: number
     followed:boolean
     fullName:string
+    photoUrl:string
     status:string
     location:{
         city:string
@@ -16,10 +17,14 @@ export type UsersType = {
 
 const initialState: InitialStateType = {
     users: [
-        {id: 1, followed:true, fullName:'Dmitry',status: 'Im ok',location:{city:'Moscow',country:'Russia'}},
-        {id: 2, followed:false, fullName:'Ivan',status: 'Im ok too',location:{city:'Minsk',country:'Belarus'}},
-        {id: 3, followed:false, fullName:'Alex',status: 'Im ok too!',location:{city:'Moscow',country:'Russia'}},
-        {id: 4, followed:false, fullName:'Andrew',status: 'OK',location:{city:'Kiev',country:'Ukraine'}}
+        {id: 1, followed:true, fullName:'Dmitry', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
+            status: 'Im ok',location:{city:'Moscow',country:'Russia'}},
+        {id: 2, followed:false, fullName:'Ivan', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
+            status: 'Im ok too',location:{city:'Minsk',country:'Belarus'}},
+        {id: 3, followed:false, fullName:'Alex', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
+            status: 'Im ok too!',location:{city:'Moscow',country:'Russia'}},
+        {id: 4, followed:false, fullName:'Andrew', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
+            status: 'OK',location:{city:'Kiev',country:'Ukraine'}}
     ]
 }
 
