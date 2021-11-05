ff9fe91f458acb6a1061ed481df5be91faecf2c7
DyatlovIvan <diatlovivan92@mail.ru>
2021-11-05 23:32:28 +0400

    49

--- src/redux/usersReducer.tsx
+++ src/redux/usersReducer.tsx
@@ -16,16 +16,7 @@ export type UsersType = {
 }
 
 const initialState: InitialStateType = {
-    users: [
-        {id: 1, followed:true, fullName:'Dmitry', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
-            status: 'Im ok',location:{city:'Moscow',country:'Russia'}},
-        {id: 2, followed:false, fullName:'Ivan', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
-            status: 'Im ok too',location:{city:'Minsk',country:'Belarus'}},
-        {id: 3, followed:false, fullName:'Alex', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
-            status: 'Im ok too!',location:{city:'Moscow',country:'Russia'}},
-        {id: 4, followed:false, fullName:'Andrew', photoUrl:'https://i.pinimg.com/474x/74/05/5f/74055f83bfbdc20fdc1f9d1fc116fd26.jpg',
-            status: 'OK',location:{city:'Kiev',country:'Ukraine'}}
-    ]
+    users: []
 }
 
 export const usersReducer = (state = initialState, action: ActionsTypes): InitialStateType => {
