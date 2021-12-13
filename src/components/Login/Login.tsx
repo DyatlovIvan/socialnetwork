c3fe3247b6e1378631f9fcb90bde9368e73d9b75
DyatlovIvan <diatlovivan92@mail.ru>
2021-12-13 14:31:32 +0400

    75 formik

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,58 @@
-export const Login = ()=>{
+import {ErrorMessage, Field, Form, Formik, useFormik} from "formik";
+type valuesType = {
+    login: string
+    password: string
+    rememberMe:boolean
+}
 
-    return(
-        <h1>LOGIN</h1>
+export const Login = () => {
+    return (
+        <div>
+            <h1>LOGIN</h1>
+            <LoginForm/>
+        </div>
     )
+}
+
+
+const LoginForm = () => {
+    const validate = (values: any) => {
+        const errors = {};
+        return errors;
+    }
+    const onSubmit = (values: valuesType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
+        debugger
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 400);
+    }
+
+    return (
+        <div>
+            <Formik
+                initialValues={{login: '', password: '',rememberMe:false}}
+                validate={validate}
+                onSubmit={onSubmit}
+            >
+                {({isSubmitting}) => (
+                    <Form>
+                        <div>
+                            <Field type="login" name="login"/>
+                            <ErrorMessage name="login" component="div"/>
+                        </div>
+                        <div><Field type="password" name="password"/>
+                            <ErrorMessage name="password" component="div"/>
+                        </div>
+                        <div>
+                            <Field type = "checkbox" name = "rememberMe"/> remember me
+                        </div>
+                        <button type="submit" disabled={isSubmitting}>
+                            Login
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>)
+
 }
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,14 +3,14 @@ import {profilePageReducer} from "./profilePageReducer";
 import {dialogsPageReducer} from "./dialogsPageReducer";
 import {usersReducer} from "./usersReducer";
 import {authReducer} from "./authReducer";
-// import thunkMiddleware from "redux-thunk";
 import thunk from "redux-thunk";
 
 let reducers = combineReducers({
     profilePage: profilePageReducer,
     dialogsPage: dialogsPageReducer,
     usersPage: usersReducer,
-    auth:authReducer
+    auth:authReducer,
+
 })
 let store = createStore(reducers,applyMiddleware(thunk));
 
